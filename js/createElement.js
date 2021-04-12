9672885706f872098842835482693038a6f9aad2
Michael Hefner <michael.hefner@outlook.com>
2021-04-11 20:54:27 -0400

    Code commenting/documentation

--- js/createElement.js
+++ js/createElement.js
@@ -1,5 +1,15 @@
 const createElement = (doc, parent, docData) => {
+  /**************************************
+   * Create element on document loaded
+   * in arguments.
+   *************************************/
+
   const newElement = doc.createElement(docData.tag);
+
+  /**************************************
+   * Create style if one exists
+   *************************************/
+
   if (docData.style) {
     docData.style.forEach((element) => {
       for (let el in element) {
@@ -7,12 +17,32 @@ const createElement = (doc, parent, docData) => {
       }
     });
   }
+
+  /**************************************
+   * Create href if one exists
+   *************************************/
   if (docData.href) {
     newElement.setAttribute("href", docData.href);
   }
+
+  /**************************************
+   * Create ID if one exists
+   *************************************/
   if (docData.id) newElement.setAttribute("id", docData.id);
+
+  /**************************************
+   * Create click listener
+   *************************************/
   newElement.addEventListener("click", docData.click);
+
+  /**************************************
+   * Create content
+   *************************************/
   newElement.innerText = docData.content || "";
+
+  /**************************************
+   * Attach element to parent
+   *************************************/
   document.querySelector(parent).appendChild(newElement);
 };
 
